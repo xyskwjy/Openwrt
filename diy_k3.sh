fe830e8820b74e54bcbe2423d3263f4d2df4f0b9
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2020-12-21 19:52:42 +0800

    Create diy_k3.sh

---
+++ diy_k3.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+
+# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
+sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+
+# 修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
+# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+
+# 取消掉feeds.conf.default文件里面的helloworld的注释，使用源码自带ShadowSocksR Plus+出国软件
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 编译K3的时候会出很多其他牌子路由器固件的，这个代码使用之后只出K3固件
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
+
+# 添加软件源
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+# sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+# git clone https://github.com/jerrykuku/node-request.git package/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# git clone https://github.com/xyskwjy/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
+git clone https://github.com/toss-a/filebrowser.git package/filebrowser
+git clone https://github.com/toss-a/luci-app-filebrowser.git package/luci-app-filebrowser
+
+#passwall出国软件
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/syncthing package/syncthing
