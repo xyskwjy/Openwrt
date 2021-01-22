896ed6a6e96aa3aae8fe7ba327957132d652c55e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-01-22 09:42:54 +0800

    Update and rename diy_k3.sh to diy_4.19.sh

--- diy_4.19.sh
+++ diy_4.19.sh
@@ -2,14 +2,11 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
 # 修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
-# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
 
 # 取消掉feeds.conf.default文件里面的helloworld的注释，使用源码自带ShadowSocksR Plus+出国软件
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 编译K3的时候会出很多其他牌子路由器固件的，这个代码使用之后只出K3固件
-sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
 
 # 添加软件源
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
@@ -18,9 +15,9 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES 
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-# git clone https://github.com/jerrykuku/node-request.git package/node-request
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 # git clone https://github.com/xyskwjy/luci-app-koolproxyR.git package/luci-app-koolproxyR
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone https://github.com/toss-a/filebrowser.git package/filebrowser
