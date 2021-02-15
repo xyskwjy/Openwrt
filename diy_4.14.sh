8890bbc74779d8e87f7658b68eb38f6a51937293
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-02-15 20:50:54 +0800

    Update diy_4.14.sh

--- diy_4.14.sh
+++ diy_4.14.sh
@@ -7,22 +7,22 @@
 # sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
-# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-# git clone https://github.com/xyskwjy/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
-git clone https://github.com/toss-a/filebrowser.git package/filebrowser
-git clone https://github.com/toss-a/luci-app-filebrowser.git package/luci-app-filebrowser
+git clone https://github.com/tty228/luci-app-serverchan package/diy/luci-app-serverchan
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/diy/luci-app-poweroff
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/diy/luci-app-jd-dailybonus
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/diy/luci-theme-atmaterial
+# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/diy/luci-theme-opentopd
+# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/diy/luci-theme-edge
+# git clone https://github.com/xyskwjy/luci-app-koolproxyR.git package/diy/luci-app-koolproxyR
+git clone https://github.com/garypang13/luci-app-eqos.git package/diy/luci-app-eqos
+git clone https://github.com/toss-a/filebrowser.git package/diy/filebrowser
+git clone https://github.com/toss-a/luci-app-filebrowser.git package/diy/luci-app-filebrowser
 
 #passwall出国软件
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/syncthing package/syncthing
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/diy/luci-app-passwall
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/diy/brook
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/diy/chinadns-ng
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/diy/tcping
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/diy/trojan-go
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/diy/trojan-plus
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/syncthing package/diy/syncthing
