8f2eb76fbabb22aaacea1d74cc286f727445250e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-03-03 00:41:09 +0800

    Update diy_5.4.sh

--- diy_5.4.sh
+++ diy_5.4.sh
@@ -15,7 +15,7 @@
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-# git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
+git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 # git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
 
