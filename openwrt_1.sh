455cae45d71e624e8e0267db464242ae7baa9fc6
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-03-04 21:54:54 +0800

    Update and rename lienol_1.sh to openwrt_1.sh

--- openwrt_1.sh
+++ openwrt_1.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
 # 添加软件源
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lean https://github.com/coolsnowwolf/lede/tree/master/package/lean' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
@@ -10,4 +10,4 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-a
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
-git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
+# git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
