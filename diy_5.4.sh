5895be861f5aa76ab09d9e44f97bf6f0298da147
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-03-12 01:59:40 +0800

    Update diy_5.4.sh

--- diy_5.4.sh
+++ diy_5.4.sh
@@ -17,7 +17,9 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-a
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 #passwall出国软件
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
