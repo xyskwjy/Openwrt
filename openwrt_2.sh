7ad3a25383b524bf628bba6ebe0172edd1ec2fd7
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-03-11 21:39:26 +0800

    Update openwrt_2.sh

--- openwrt_2.sh
+++ openwrt_2.sh
@@ -4,4 +4,7 @@
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
+rm -rf ./package/lean/luci-app-jd-dailybonus
