c9c7714be578ee7b6a8453cfc12257395e30825a
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-03-12 01:47:07 +0800

    Update openwrt_1.sh

--- openwrt_1.sh
+++ openwrt_1.sh
@@ -17,5 +17,5 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-a
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
-git clone https://github.com/garypang13/luci-theme-edge/tree/18.06 package/luci-theme-edge
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
 
