291bf7d94af4fa05ee1c93f79d9394b7790778ff
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-03-14 20:17:37 +0800

    Update openwrt_1.sh

--- openwrt_1.sh
+++ openwrt_1.sh
@@ -18,4 +18,5 @@ git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-a
 # git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 
-https://github.com/Lienol/openwrt/tree/21.02/package/lean/luci-app-wrtbwmon
+rm -rf ./package/lean/luci-app-wrtbwmon
+git clone -b 18.06 
