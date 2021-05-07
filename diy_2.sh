b8642fe8c29b07b898c65c116b79b061e022d94d
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-05-08 02:48:15 +0800

    Update diy_2.sh

--- diy_2.sh
+++ diy_2.sh
@@ -6,5 +6,3 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 
 # 设置密码为空
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
-rm -rf ./package/lean/luci-app-jd-dailybonus
