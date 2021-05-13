54a576b402d785e9dfebb4af66be107dbf5d60e2
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-05-13 13:06:16 +0800

    Update diy_5.4.sh

--- diy_5.4.sh
+++ diy_5.4.sh
@@ -18,4 +18,4 @@ rm -rf ./package/lean/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 rm -rf ./feeds/packages/net/miniupnpd
-svn co https://github.com/Ljzkirito/openwrt-packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
+svn co https://github.com/Ljzkirito/openwrt-packages/trunk/main/miniupnpd feeds/packages/net/miniupnpd
