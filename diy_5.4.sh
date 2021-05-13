44d219a436c9a2da9b3366c9257a83cb88817b96
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-05-13 11:11:33 +0800

    Update diy_5.4.sh

--- diy_5.4.sh
+++ diy_5.4.sh
@@ -14,6 +14,7 @@
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-rm -rf ./package/lean/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+
+git clone https://github.com/Ljzkirito/openwrt-packages/trunk/miniupnpd packages/miniupnp
