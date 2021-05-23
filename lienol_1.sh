25135bd5b210741b536b78e689b5ebb03d449d89
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-05-23 14:15:33 +0800

    Update lienol_1.sh

--- lienol_1.sh
+++ lienol_1.sh
@@ -14,5 +14,3 @@ git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-a
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
 git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
-rm -rf ./feeds/packages/net/miniupnpd
-svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
