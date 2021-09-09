6376b5202db54062364db3c72455721b498dcc9d
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-09 22:13:14 +0800

    Update diy_2.sh

--- diy_2.sh
+++ diy_2.sh
@@ -8,7 +8,7 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 更新miniupnp版本
-rm -fr feeds/packages/net/miniupnpd
-svn co https://github.com/Ljzkirito/openwrt-packages/trunk/miniupnpd feeds/packages/net/miniupnpd
-rm -fr feeds/luci/applications/luci-app-upnp
-svn co https://github.com/Ljzkirito/openwrt-packages/trunk/luci-app-upnp feeds/luci/applications/luci-app-upnp
+# rm -fr feeds/packages/net/miniupnpd
+# svn co https://github.com/Ljzkirito/openwrt-packages/trunk/miniupnpd feeds/packages/net/miniupnpd
+# rm -fr feeds/luci/applications/luci-app-upnp
+# svn co https://github.com/Ljzkirito/openwrt-packages/trunk/luci-app-upnp feeds/luci/applications/luci-app-upnp
