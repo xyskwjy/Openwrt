603ef573af11e67478da769299e3d329bd6d055c
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-18 22:07:06 +0800

    Update lienol_1.sh

--- lienol_1.sh
+++ lienol_1.sh
@@ -13,6 +13,6 @@ sed -i 's/KERNEL_TESTING_PATCHVER:=4.9/KERNEL_TESTING_PATCHVER:=4.14/g' ./target
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+# git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 # git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
