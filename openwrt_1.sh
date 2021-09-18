12ab1fb2d960f83cb4ee32a8da5371d31c5da03e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-19 00:19:12 +0800

    Update openwrt_1.sh

--- openwrt_1.sh
+++ openwrt_1.sh
@@ -10,6 +10,8 @@
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+# git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 # git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
