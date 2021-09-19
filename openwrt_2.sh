1c5ec7f83fdb0eb79c735cd65b0341412ed0d789
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-19 13:25:56 +0800

    Delete openwrt_2.sh

--- openwrt_2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-
-# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
-
-# 设置密码为空
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ  
