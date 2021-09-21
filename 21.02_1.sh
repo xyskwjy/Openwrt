983e4ac11eef89f36f73bd3c44db6f28f99be36f
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-21 22:19:05 +0800

    Create 21.02_1.sh

---
+++ 21.02_1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
