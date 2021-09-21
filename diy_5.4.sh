6b466650ac513a142cc940a51b4c5e6bac1bb7c3
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-21 23:31:24 +0800

    Update diy_5.4.sh

--- diy_5.4.sh
+++ diy_5.4.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
 # 修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
-# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # 取消掉feeds.conf.default文件里面的helloworld的注释，使用源码自带ShadowSocksR Plus+出国软件
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
