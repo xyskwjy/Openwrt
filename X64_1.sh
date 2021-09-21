98accd4c45a0f248f131387b01c42388d9f1b49e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 03:34:53 +0800

    Update X64_1.sh

--- X64_1.sh
+++ X64_1.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
-# 修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
-sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
-
-# 取消掉feeds.conf.default文件里面的helloworld的注释，使用源码自带ShadowSocksR Plus+出国软件
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 添加软件源
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-# git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 
-#关闭 禁止解析 IPv6 DNS 记录
-sed -i 's/option filter_aaaa\t1/option filter_aaaa\t0/g' package/network/services/dnsmasq/files/dhcp.conf
-
-# Add luci-app-dockerman
-# rm -rf ../lean/luci-app-docker
-# git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-# git clone --depth=1 https://github.com/lisaac/luci-lib-docker
