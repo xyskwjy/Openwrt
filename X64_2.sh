8355cf3ff43009f79c172b39011d7925cd3d5884
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 03:36:49 +0800

    Update X64_2.sh

--- X64_2.sh
+++ X64_2.sh
@@ -4,11 +4,8 @@
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
-# 设置密码为空
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
-# 更新miniupnp版本
-# rm -fr feeds/packages/net/miniupnpd
-# svn co https://github.com/Ljzkirito/openwrt-packages/trunk/miniupnpd feeds/packages/net/miniupnpd
-# rm -fr feeds/luci/applications/luci-app-upnp
-# svn co https://github.com/Ljzkirito/openwrt-packages/trunk/luci-app-upnp feeds/luci/applications/luci-app-upnp
+# Add luci-app-dockerman
+rm -rf package/diy/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
+rm -rf package/diy/luci-lib-docker
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
