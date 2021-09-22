37880c7b21c86557660e6bb0dff45a4e7cab2137
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 13:53:54 +0800

    Update 21.02_2.sh

--- 21.02_2.sh
+++ 21.02_2.sh
@@ -6,3 +6,7 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 
 # 设置密码为空
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ
+
+# Add luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
