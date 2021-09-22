51556825904e5701655b075438101c134fda15be
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 12:59:36 +0800

    Update 21.02_1.sh

--- 21.02_1.sh
+++ 21.02_1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx
 
+# 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
