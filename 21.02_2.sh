df47af58485fb0ed063c4b2a92da49325c6e635d
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 13:32:26 +0800

    Update 21.02_2.sh

--- 21.02_2.sh
+++ 21.02_2.sh
@@ -4,6 +4,5 @@
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ../default-settings/files/zzz-default-settings
