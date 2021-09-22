a533e0aa8974eb9f6c96eab6fde4039bae63b6ec
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 13:56:05 +0800

    Update 21.02_1.sh

--- 21.02_1.sh
+++ 21.02_1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx
 
+# 修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
+# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
+
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
