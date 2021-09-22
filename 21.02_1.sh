54ad4c235c91ba75f24b6d2dc11aeaa13b2f2112
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-22 13:01:02 +0800

    Update 21.02_1.sh

--- 21.02_1.sh
+++ 21.02_1.sh
@@ -4,6 +4,10 @@
 # 设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
+# Add luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
+
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
