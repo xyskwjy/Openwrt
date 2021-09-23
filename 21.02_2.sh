39435c9a5c8c25125a61c534157e5e49101f782c
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-23 21:18:17 +0800

    Update 21.02_2.sh

--- 21.02_2.sh
+++ 21.02_2.sh
@@ -5,10 +5,14 @@
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认语言
-sed -i 's/option lang auto/option lang zh_cn/g' feeds/luci/blob/master/modules/luci-base/root/etc/config/luci
+uci set luci.main.lang=zh_cn
+uci commit luci
+
+uci set system.@system[0].timezone=CST-8
+uci set system.@system[0].zonename=Asia/Shanghai
 
 # 设置密码为空
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
 # Add luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
