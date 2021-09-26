82371984a51f1adefeaff8946329f6e9ce9ff4a9
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-09-26 10:30:38 +0800

    Update 21.02_2.sh

--- 21.02_2.sh
+++ 21.02_2.sh
@@ -4,14 +4,29 @@
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
-# 修改默认语言
+cat >$NETIP <<-EOF
 uci set luci.main.lang=zh_cn
 uci commit luci
-
 uci set system.@system[0].timezone=CST-8
 uci set system.@system[0].zonename=Asia/Shanghai
+uci -q batch
+	delete system.@system[0]
+	add system system
+	set system.@system[-1].hostname='OpenWrt'
+	set system.@system[-1].timezone='UTC'
+	set system.@system[-1].ttylogin='0'
+	set system.@system[-1].log_size='64'
+	set system.@system[-1].urandom_seed='0'
+	delete system.ntp
+	set system.ntp='timeserver'
+	set system.ntp.enabled='1'
+	set system.ntp.enable_server='0'
+	add_list system.ntp.server='0.openwrt.pool.ntp.org'
+	add_list system.ntp.server='1.openwrt.pool.ntp.org'
+	add_list system.ntp.server='2.openwrt.pool.ntp.org'
+	add_list system.ntp.server='3.openwrt.pool.ntp.org'
+EOF
 
-# 设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
 # Add luci-app-dockerman
