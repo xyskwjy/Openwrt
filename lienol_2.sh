96e84f2c532394eab6cae8337953a5f9020aa2d0
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-10-15 11:04:34 +0800

    Update lienol_2.sh

--- lienol_2.sh
+++ lienol_2.sh
@@ -3,9 +3,3 @@
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
-
-# Add luci-app-dockerman
-rm -rf package/diy/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
-rm -rf package/diy/luci-lib-docker
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
