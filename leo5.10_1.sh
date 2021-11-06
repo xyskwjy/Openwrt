8cbae936e6a75a42dc71f116410b6b336244e653
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-11-06 13:13:11 +0800

    Update and rename 21.02_1.sh to leo5.10_1.sh

--- leo5.10_1.sh
+++ leo5.10_1.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx
 
 # 修改版本内核
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
-# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 #添加自定义插件链接
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
