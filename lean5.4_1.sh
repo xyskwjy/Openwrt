a803f997845588020549c41fc8ce6c87b6a3ad60
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-11-09 13:10:56 +0800

    Update lean5.4_1.sh

--- lean5.4_1.sh
+++ lean5.4_1.sh
@@ -14,28 +14,28 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-a
 git clone -b 18.06 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 
 # Add luci-app-dockerman
-rm -rf package/lean/luci-app-docker
+rm -rf ./package/lean/luci-app-docker
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-rm -rf package/lean/luci-lib-docker
+rm -rf ./package/lean/luci-lib-docker
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 #Remove default apps
-sed -i 's/luci-app-zerotier//g' target/linux/x86/Makefile
-sed -i 's/luci-app-qbittorrent//g' target/linux/x86/Makefile
-sed -i 's/luci-app-uugamebooster//g' target/linux/x86/Makefile
-sed -i 's/ddns-scripts_dnspod//g' target/linux/x86/Makefile
-sed -i 's/ddns-scripts_aliyun//g' target/linux/x86/Makefile
-sed -i 's/luci-app-xlnetacc//g' target/linux/x86/Makefile
-sed -i 's/luci-app-unblockmusic//g' target/linux/x86/Makefile
-sed -i 's/luci-app-ipsec-vpnd//g' target/linux/x86/Makefile
-sed -i 's/luci-app-adbyby-plus//g' target/linux/x86/Makefile
-sed -i 's/luci-app-vsftpd//g' include/target.mk
-sed -i 's/luci-app-unblockmusic//g' include/target.mk
-sed -i 's/luci-app-wol//g' include/target.mk
-sed -i 's/luci luci-app-ddns//g' include/target.mk
-sed -i 's/luci-app-accesscontrol//g' include/target.mk
-sed -i 's/luci-app-autoreboot//g' include/target.mk
-sed -i 's/luci-app-ssr-plus//g' include/target.mk
-sed -i 's/ddns-scripts_aliyun//g' include/target.mk
-sed -i 's/ddns-scripts_dnspod//g' include/target.mk
-sed -i 's/luci-app-nlbwmon//g' include/target.mk
+sed -i 's/luci-app-zerotier//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-qbittorrent//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-uugamebooster//g' ./target/linux/x86/Makefile
+sed -i 's/ddns-scripts_dnspod//g' ./target/linux/x86/Makefile
+sed -i 's/ddns-scripts_aliyun//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-xlnetacc//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-unblockmusic//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-ipsec-vpnd//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-adbyby-plus//g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-vsftpd//g' ./include/target.mk
+sed -i 's/luci-app-unblockmusic//g' ./include/target.mk
+sed -i 's/luci-app-wol//g' ./include/target.mk
+sed -i 's/luci luci-app-ddns//g' ./include/target.mk
+sed -i 's/luci-app-accesscontrol//g' ./include/target.mk
+sed -i 's/luci-app-autoreboot//g' ./include/target.mk
+sed -i 's/luci-app-ssr-plus//g' ./include/target.mk
+sed -i 's/ddns-scripts_aliyun//g' ./include/target.mk
+sed -i 's/ddns-scripts_dnspod//g' ./include/target.mk
+sed -i 's/luci-app-nlbwmon//g' ./include/target.mk
