bc18a0adb52fd021c7f427dfbc626fcb526dbdf3
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-11-09 13:18:13 +0800

    Update lienol_main_1.sh

--- lienol_main_1.sh
+++ lienol_main_1.sh
@@ -8,9 +8,9 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # 替换luci-app-dockerman
-rm -rf ./package/diy/luci-app-dockerman
+rm -rf package/diy/luci-app-dockerman
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-rm -rf ./package/diy/luci-lib-docker
+rm -rf package/diy/luci-lib-docker
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
@@ -19,18 +19,17 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-a
 git clone -b 18.06 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 
 #Remove default apps
-rm -rf ./package/diy/OpenAppFilter
-sed -i 's/ddns-scripts_dnspod//g' ./target/linux/x86/Makefile
-sed -i 's/ddns-scripts_aliyun//g' ./target/linux/x86/Makefile
-sed -i 's/luci-app-usb-printer//g' ./target/linux/x86/Makefile
-sed -i 's/luci-app-vsftpd//g' ./target/linux/x86/Makefile
-sed -i 's/luci-app-wol//g' ./include/target.mk
-sed -i 's/luci-app-ddns//g' ./include/target.mk
-sed -i 's/ddns-scripts-cloudflare//g' ./include/target.mk
-sed -i 's/ddns-scripts_aliyun//g' ./include/target.mk
-sed -i 's/ddns-scripts_dnspod//g' ./include/target.mk
-sed -i 's/luci-app-control-timewol//g' ./include/target.mk
-sed -i 's/luci-app-control-webrestriction//g' ./include/target.mk
-sed -i 's/luci-app-control-weburl//g' ./include/target.mk
-sed -i 's/luci-app-timecontrol//g' ./include/target.mk
+sed -i 's/ddns-scripts_dnspod//g' target/linux/x86/Makefile
+sed -i 's/ddns-scripts_aliyun//g' target/linux/x86/Makefile
+sed -i 's/luci-app-usb-printer//g' target/linux/x86/Makefile
+sed -i 's/luci-app-vsftpd//g' target/linux/x86/Makefile
+sed -i 's/luci-app-wol//g' include/target.mk
+sed -i 's/luci-app-ddns//g' include/target.mk
+sed -i 's/ddns-scripts-cloudflare//g' include/target.mk
+sed -i 's/ddns-scripts_aliyun//g' include/target.mk
+sed -i 's/ddns-scripts_dnspod//g' include/target.mk
+sed -i 's/luci-app-control-timewol//g' include/target.mk
+sed -i 's/luci-app-control-webrestriction//g' include/target.mk
+sed -i 's/luci-app-control-weburl//g' include/target.mk
+sed -i 's/luci-app-timecontrol//g' include/target.mk
 
