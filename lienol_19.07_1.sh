0b1c02a43d065ad726c450f0d22a3644ce38a381
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-11-10 23:13:05 +0800

    Update lienol_19.07_1.sh

--- lienol_19.07_1.sh
+++ lienol_19.07_1.sh
@@ -10,15 +10,15 @@ sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' ./target/linux/x86/Makef
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-dockerman
-rm -rf package/diy/luci-app-dockerman
-git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-rm -rf package/diy/luci-lib-docker
-git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+# rm -rf package/diy/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+# rm -rf package/diy/luci-lib-docker
+# git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone -b 18.06 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
+# git clone -b 18.06 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 
 #Remove default apps
 sed -i 's/ddns-scripts_dnspod//g' target/linux/x86/Makefile
