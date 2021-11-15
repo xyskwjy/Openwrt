c81b9213ba066d4fbb5370a9e0e64a5e7a2a269a
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-11-16 03:35:09 +0800

    Update Official1.sh

--- Official1.sh
+++ Official1.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part01.sh
+
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
