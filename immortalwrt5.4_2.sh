a02903ca88957101ba8ccb4f0ebe1854982c84c2
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-11-16 18:53:50 +0800

    Update immortalwrt5.4_2.sh

--- immortalwrt5.4_2.sh
+++ immortalwrt5.4_2.sh
@@ -6,11 +6,14 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
+# File name: immortalwrt5.4_2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/emortal/default-settings/files/zzz-default-settings
+
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
