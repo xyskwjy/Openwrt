0ddb73639d3103c7cc8b63a38c357abab68926e6
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-04 10:52:34 +0800

    Update lean5.4_1.sh

--- lean5.4_1.sh
+++ lean5.4_1.sh
@@ -21,6 +21,11 @@
 rm -rf package/network/services/ppp
 svn co https://github.com/openwrt/openwrt/trunk/package/network/services/ppp package/network/services/ppp
 
+# Add luci-app-dockerman
+rm -rf package/lean/luci-app-docker package/lean/luci-lib-docker
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman  package/lean/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/lean/luci-lib-docker
+
 #添加额外软件包
 # git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/openwrt-iptvhelper
