cc598984c04d1fdaa75420bc35fced9dea23b576
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-13 10:28:09 +0800

    Update lean5.4_2.sh

--- lean5.4_2.sh
+++ lean5.4_2.sh
@@ -19,8 +19,8 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 # 更新miniupnp版本
-rm -fr feeds/packages/net/miniupnpd
-svn co https://github.com/Ljzkirito/openwrt-packages/trunk/miniupnpd feeds/packages/net/miniupnpd
-rm -fr feeds/luci/applications/luci-app-upnp
-svn co https://github.com/Ljzkirito/openwrt-packages/trunk/luci-app-upnp feeds/luci/applications/luci-app-upnp
+# rm -fr feeds/packages/net/miniupnpd
+# svn co https://github.com/Ljzkirito/openwrt-packages/trunk/miniupnpd feeds/packages/net/miniupnpd
+# rm -fr feeds/luci/applications/luci-app-upnp
+# svn co https://github.com/Ljzkirito/openwrt-packages/trunk/luci-app-upnp feeds/luci/applications/luci-app-upnp
 
