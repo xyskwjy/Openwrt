26919adee084edc900477ed4d7cbd294b43cbf63
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-19 17:08:58 +0800

    Update lean5.4_1.sh

--- lean5.4_1.sh
+++ lean5.4_1.sh
@@ -14,17 +14,6 @@
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 使用官方ppp 
-# rm -rf package/network/services/ppp
-# svn co https://github.com/openwrt/openwrt/trunk/package/network/services/ppp package/network/services/ppp
-
 #添加额外软件包
 # git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/openwrt-iptvhelper
@@ -34,4 +23,4 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/l
 # 去除默认软件
 sed -i 's/luci-app-wireguard//g' target/linux/x86/Makefile
 
-rm -rf package/lean/r8152/patches/010-compat-skb_mark_not_on_list-was-backported-since-ker.patch
+# rm -rf package/lean/r8152/patches/010-compat-skb_mark_not_on_list-was-backported-since-ker.patch
