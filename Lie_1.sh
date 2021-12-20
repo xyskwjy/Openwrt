54aa287ab660134a0283bab75a7874d6a3304509
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-20 18:50:40 +0800

    Update Lie_1.sh

--- Lie_1.sh
+++ Lie_1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Copyright (c) 2019-2020 P3TERX <https://p3terx
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: Lie_1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
 
