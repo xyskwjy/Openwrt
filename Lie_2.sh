d73d4e617c8adac25460a4636079f66bbeacb06e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-20 18:46:58 +0800

    Create Lie_2.sh

---
+++ Lie_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+
+# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
+sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+
+# 版本号里显示一个自己的名字
+sed -i "s/OpenWrt/xywjy build $(TZ=UTC-8 date "+%y.%m.%d") @/g" package/default-settings/files/zzz-default-settings
+
+#修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
