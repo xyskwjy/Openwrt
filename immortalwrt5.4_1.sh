667304b8f2a45babd45a34a9ab1ed05a872ac488
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-20 08:27:41 +0800

    Update immortalwrt5.4_1.sh

--- immortalwrt5.4_1.sh
+++ immortalwrt5.4_1.sh
@@ -9,6 +9,8 @@
 # File name: immortalwrt5.4_1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
