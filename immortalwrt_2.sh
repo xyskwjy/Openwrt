a6c56f7749e224831a04de99cd3f9bf455da9c9b
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-23 23:48:04 +0800

    Update immortalwrt_2.sh

--- immortalwrt_2.sh
+++ immortalwrt_2.sh
@@ -9,9 +9,6 @@
 # File name: immortalwrt5.4_2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# 设置密码为空
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
