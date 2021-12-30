a2f865a6fb0c7ffe18ccffbd7ef5558e0773ae6e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2021-12-31 01:55:23 +0800

    Update immortalwrt_1.sh

--- immortalwrt_1.sh
+++ immortalwrt_1.sh
@@ -9,7 +9,7 @@
 # File name: immortalwrt_1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
