45d82325107482bbcb0427bcfef35ca3de5cb009
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2022-01-06 12:11:12 +0800

    Update lean_1.sh

--- lean_1.sh
+++ lean_1.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改版本内核
-# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 #添加额外软件包
 # git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
