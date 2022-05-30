7a901e3b475e068e806a2fb6d9587ed5c125cef9
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2022-05-30 22:51:53 +0800

    Update Lie_1.sh

--- Lie_1.sh
+++ Lie_1.sh
@@ -9,7 +9,7 @@
 # File name: Lie_1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=4.14/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # 取消掉feeds.conf.default文件里面的helloworld的注释，使用源码自带ShadowSocksR Plus+出国软件
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -18,4 +18,4 @@
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 # git clone -b 18.06 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
+# git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
