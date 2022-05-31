cae0e5a55255301ab99c5e3ea405839089f91766
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2022-05-31 14:18:59 +0800

    Update Lie_2.sh

--- Lie_2.sh
+++ Lie_2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 # 版本号里显示一个自己的名字
 sed -i "s/OpenWrt/xywjy build $(TZ=UTC-8 date "+%y.%m.%d") @/g" package/default-settings/files/zzz-default-settings
