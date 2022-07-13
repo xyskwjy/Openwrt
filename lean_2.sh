57815f5fafdd961556e941f3ea7b2e93ceedefbc
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2022-07-13 12:52:07 +0800

    Update lean_2.sh

--- lean_2.sh
+++ lean_2.sh
@@ -21,3 +21,5 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
+sed -i 's/kmod-igb-intel//g' target/linux/x86/Makefile
+
