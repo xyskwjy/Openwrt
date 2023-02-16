cfd8a655b79e13213d3b204577183380882f0189
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2023-02-16 14:15:19 +0800

    Update lean_2.sh

--- lean_2.sh
+++ lean_2.sh
@@ -19,6 +19,6 @@ sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=''/g" package/lean/default-set
 # sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 #修正连接数（by ベ七秒鱼ベ）
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 
