17843844ac7e3fbb196c64dc634a255419da27c5
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2024-07-31 01:09:08 +0800

    Update L6.1.1.sh

--- L6.1.1.sh
+++ L6.1.1.sh
@@ -14,10 +14,9 @@
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 #添加额外软件包
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-# git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/openwrt-iptvhelper
-git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+git clone git clone -b main https://github.com/ilxp/luci-app-ikoolproxy.git package/lean/luci-app-ikoolproxy
+# git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
 
 
