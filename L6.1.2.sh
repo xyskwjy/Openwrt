2e213ad0e345f8a9531aa75f3fc9fc1c283efc20
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2025-05-28 21:09:45 +0800

    Update L6.1.2.sh

--- L6.1.2.sh
+++ L6.1.2.sh
@@ -16,7 +16,7 @@ sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='Linux-OP6.6-$(date +%Y%
 sed -i "s/hostname='OpenWrt'/hostname='Linux-op'/g" ./package/base-files/files/bin/config_generate
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 #修正连接数（by ベ七秒鱼ベ）
 # sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
