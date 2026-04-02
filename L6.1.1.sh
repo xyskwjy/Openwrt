466915011e207221d8675db4732a1bec2ec8de64
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2026-04-02 19:41:13 +0800

    Update L6.1.1.sh

--- L6.1.1.sh
+++ L6.1.1.sh
@@ -14,8 +14,8 @@
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 #添加额外软件包
-# git clone git clone -b main https://github.com/ilxp/luci-app-ikoolproxy.git package/lean/luci-app-ikoolproxy
-# git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
-# git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
-
+# git clone https://github.com/vernesong/OpenClash.git package/diy/OpenClash
+git clone -b main https://github.com/ilxp/luci-app-ikoolproxy.git package/diy/luci-app-ikoolproxy
+git clone https://github.com/timsaya/openwrt-bandix.git package/diy/openwrt-bandix
+git clone https://github.com/timsaya/luci-app-bandix.git package/diy/luci-app-bandix
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/diy/luci-app-poweroff
