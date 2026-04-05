67cb520f6d080ac412acc6eb52e8a0f55d9bdc8e
xyskwjy <61834211+xyskwjy@users.noreply.github.com>
2026-04-06 02:26:00 +0800

    Update immortalwrt_1.sh

--- immortalwrt_1.sh
+++ immortalwrt_1.sh
@@ -20,7 +20,6 @@
 
 #添加额外非必须软件包
 git clone https://github.com/sirpdboy/luci-app-parentcontrol.git package/diy/luci-app-parentcontrol
-git clone -b main https://github.com/ilxp/luci-app-ikoolproxy.git package/diy/luci-app-ikoolproxy
 git clone https://github.com/timsaya/openwrt-bandix.git package/diy/openwrt-bandix
 git clone https://github.com/timsaya/luci-app-bandix.git package/diy/luci-app-bandix
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/diy/luci-app-poweroffdevice
